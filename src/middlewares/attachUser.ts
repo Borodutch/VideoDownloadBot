793b5eba47e3591e156e9491da43cd06ee8c92b8
Nikita Kolmogorov <backmeupplz@gmail.com>
2021-11-28 09:12:09 -0800

    fix group chats

--- src/middlewares/attachUser.ts
+++ src/middlewares/attachUser.ts
@@ -3,10 +3,10 @@ import { findOrCreateChat } from '@/models/Chat'
 import Context from '@/models/Context'
 
 export default async function attachChat(ctx: Context, next: NextFunction) {
-  if (!ctx.from) {
+  if (!ctx.chat) {
     throw new Error('ctx.from is not defined')
   }
-  const { doc } = await findOrCreateChat(ctx.from.id)
+  const { doc } = await findOrCreateChat(ctx.chat.id)
   ctx.dbchat = doc
   return next()
 }
