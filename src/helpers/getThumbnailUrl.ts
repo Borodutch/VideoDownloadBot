78becccfc34603418a9582ff1aae4d90a52deb4e
Semion <babaksemion@gmail.com>
2022-03-03 01:43:29 +0300

    забыл удалить лишний код

--- src/helpers/getThumbnailUrl.ts
+++ src/helpers/getThumbnailUrl.ts
@@ -24,7 +24,7 @@ export default async function getThumbnailUrl(
       break
     }
   }
-  thumbnailUrl = ''
+
   let thumbnailPath = ''
   if (!thumbnailUrl) {
     const thumbName = `${thumbnailUuid}.jpeg`
